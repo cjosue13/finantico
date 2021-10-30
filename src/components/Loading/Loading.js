0e4ae7a255fc352a361d2016cb53cee339654478
cjosue13 <solomaj12@gmail.com>
2021-10-29 23:47:18 -0600

    fixes

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import {Heading, HStack, Spinner} from 'native-base';
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <HStack space={2} alignItems="center">
+      <Spinner accessibilityLabel="Loading posts" />
+      <Heading color="#01234c" fontSize="md">
+        Loading
+      </Heading>
+    </HStack>
+  );
+};
+
+export default Loading;
